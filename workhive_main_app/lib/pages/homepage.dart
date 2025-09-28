611e4216524b97effeb78f1c6a1b7c3df019d042
ArthitBIE <atiyutpunkeaw@gmail.com>
2025-09-28 11:30:02 +0700

    ai fix

--- workhive_main_app/lib/pages/homepage.dart
+++ workhive_main_app/lib/pages/homepage.dart
@@ -6,7 +6,9 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      
+      body: Center(
+        child: Text('Welcome to Homepage'),
+      ),
     );
   }
 }
